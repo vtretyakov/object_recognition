0aae2d2aed13eceda5b37cf491bbca3b7ad1a453
vtretyakov <vtretyakov@synapticon.com>
2018-09-09 13:15:08 +0200

    add green bounding boxes around classified traffic lights

--- test_inference.py
+++ test_inference.py
@@ -56,10 +56,11 @@ class Classifier(object):
         classes = np.squeeze(classes).astype(np.int32)
         scores = np.squeeze(scores)
         img_cnt = 1
+        tl_image = None
         for idx, classID in enumerate(classes):
             if classID == 10:
                 print ("found traffic light", scores[idx])
-                if scores[idx] > 0.5:
+                if scores[idx] > 0.30:
 
                     nbox = boxes[idx]
 
@@ -69,13 +70,16 @@ class Classifier(object):
                     box = np.array([nbox[0]*height, nbox[1]*width, nbox[2]*height, nbox[3]*width]).astype(int)
                     tl_image = image[box[0]:box[2], box[1]:box[3]]
                     img_out = cv2.cvtColor(tl_image, cv2.COLOR_RGB2BGR)
-                    cv2.imwrite('img_samples/classified' + str(img_cnt) + '.jpg', img_out)
+                    cv2.imwrite('img_samples/simulator/classified' + str(img_cnt) + '.jpg', img_out)
+                    cv2.rectangle(image, (box[1], box[0]), (box[3], box[2]), (0, 255, 0), 2)
                     img_cnt += 1
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('img_samples/simulator/classified_box.jpg', image)
         return tl_image
 
 # Execute `main()` function
 if __name__ == '__main__':
-    image_file = './img_samples/test.jpg'
+    image_file = './img_samples/simulator/6.png'
     image = scipy.misc.imread(image_file)
     classifier = Classifier()
     classifier.get_classification(image)
